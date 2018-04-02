03fa1fb259bf440ece2edff6dca8a75a533d6bf1
tsing_evan <3212932284@qq.com>
2018-04-02 20:54:32 +0800

    4-2 20:54 commit

--- src/main/java/tij/concurrents/part7/CountDownLatchDemo.java
+++ src/main/java/tij/concurrents/part7/CountDownLatchDemo.java
@@ -1,14 +1,73 @@
 package tij.concurrents.part7;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class CountDownLatchDemo {
-    public static void main(String[] args) throws UnknownHostException {
-        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("YYYYMMddHHmmss")));
-        System.out.println(String.valueOf(LocalDateTime.now()));
-        System.out.println(InetAddress.getLocalHost().getHostAddress());
+    static final int SIZE = 100;
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        CountDownLatch latch = new CountDownLatch(SIZE);
+        for(int i = 0; i < 10; i++){
+            exec.execute(new WaitingTask(latch));
+        }
+        for (int i = 0; i < SIZE; i++){
+            exec.execute(new TaskPortion(latch));
+        }
+        System.out.println("Launched all tasks");
+        TimeUnit.SECONDS.sleep(5);
+        exec.shutdownNow();
+    }
+}
+class TaskPortion implements Runnable{
+    private static int counter = 0;
+    private final int id = counter++;
+    private static Random rand = new Random(47);
+    private final CountDownLatch latch;
+
+    public TaskPortion(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public void run() {
+        try{
+            doWork();
+            latch.countDown();
+        } catch (InterruptedException e) {
+            //Acceptabel way to exit
+        }
+    }
+    public void doWork() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(rand.nextInt(2000));
+        System.out.println(this+" completed");
+    }
+    public String toString(){
+        return String.format("TaskPortion %1$-3d", id);
+    }
+}
+class WaitingTask implements Runnable{
+    private static int counter = 0;
+    private final int id = counter++;
+    private final CountDownLatch latch;
+
+    public WaitingTask(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public void run() {
+        try{
+            latch.await();
+            System.out.println("Latch barrier passed for "+this);
+        } catch (InterruptedException e) {
+            System.out.println(this+" interrupted");
+        }
+    }
+    public String toString(){
+        return String.format("WaitingTask %1$-3d", id);
     }
 }
